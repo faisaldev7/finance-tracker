ddfd6ba5d8b2a5985f8ea63954fdc7aaf0eb4c56
Faisal Anwar Nizami <faisaldev7@gmail.com>
2020-11-09 11:32:04 +0530

    Create stocks table and controller to search stocks from browser

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_140509) do
+ActiveRecord::Schema.define(version: 2020_11_08_065619) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.string "name"
+    t.string "last_price"
+    t.string "decimal"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
