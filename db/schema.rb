2201ec0590d62babfca2410eb6643de6d62b17cc
Faisal Anwar Nizami <faisaldev7@gmail.com>
2020-11-09 11:54:31 +0530

    Create M2M Association b/w users and stocks & setup UserStock resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_065619) do
+ActiveRecord::Schema.define(version: 2020_11_09_060841) do
 
   create_table "stocks", force: :cascade do |t|
     t.string "ticker"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2020_11_08_065619) do
     t.string "decimal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_stocks", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "stock_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["stock_id"], name: "index_user_stocks_on_stock_id"
+    t.index ["user_id"], name: "index_user_stocks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +42,6 @@ ActiveRecord::Schema.define(version: 2020_11_08_065619) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_stocks", "stocks"
+  add_foreign_key "user_stocks", "users"
 end
