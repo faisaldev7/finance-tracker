7a06b51667022b0890f7ec8206a9b1c9d6c55188
Faisal Anwar Nizami <faisaldev7@gmail.com>
2020-11-10 18:49:40 +0530

    Add Friend functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#friends_list'
-
+  get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
+  resources :users, only: [:show]
 end
